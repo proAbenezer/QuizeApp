522bb1a308afb3c5ccab63059d07c40a96b19e9a
Abenezer Haile <devabenezer7@gmail.com>
2024-07-02 09:31:57 +0300

    Refactor quiz app for modularity, improved input validation, feedback, dynamic scoring, and readability

--- Quiz.js
+++ Quiz.js
@@ -1,86 +1,81 @@
+const quizQuestion = require("./data.js");
 const prompt = require("prompt-sync")();
-const quizQuestion = [
-  {
-    question:
-      "Which programming language is known for its use in web development and is commonly paired with Node.js?",
-    options: ["Python", "Java", "JavaScript", "C++"],
-    correctAnswer: "JavaScript",
-  },
-  {
-    question:
-      "In 'The Walking Dead,' who is the leader of the group at the beginning of the series?",
-    options: ["Rick Grimes", "Daryl Dixon", "Michonne", "Glenn Rhee"],
-    correctAnswer: "Rick Grimes",
-  },
-  {
-    question: "Who was the first president of the United States?",
-    options: [
-      "Thomas Jefferson",
-      "John Adams",
-      "George Washington",
-      "Abraham Lincoln",
-    ],
-    correctAnswer: "George Washington",
-  },
-  {
-    question:
-      "Which book of the Bible tells the story of the creation of the world?",
-    options: ["Exodus", "Genesis", "Leviticus", "Numbers"],
-    correctAnswer: "Genesis",
-  },
-
-  {
-    question: "During which war was the Battle of Gettysburg fought?",
-    options: [
-      "World War I",
-      "World War II",
-      "American Civil War",
-      "Vietnam War",
-    ],
-    correctAnswer: "American Civil War",
-  },
-];
 
 let scores = 0;
+let userQuit = false;
+quizQuestion.forEach((item, index) => {
+  if (userQuit === true) return;
+  let userAnswer;
 
-quizQuestion.map((item) => {
-  console.log(item.question);
+  displayQuestion(item, index);
+  let userInput = prompt(
+    "Please Enter you answer (A,B,C,D) or Q to quit the game:  "
+  );
+
+  while (!checkInputValidate(userInput)) {
+    console.log("You have entered an invaild input ");
+    userInput = prompt(
+      "Please Enter you answer (A,B,C,D) or Q to quit the game:  "
+    );
+  }
+  if (userInput.toUpperCase() === "Q") {
+    console.log("You have Quit the Game");
+    userQuit = true;
+    return;
+  }
+
+  userAnswer = readAnswer(userInput, item);
+
+  if (userAnswer == item.correctAnswer) scores++;
+});
+
+console.log("");
+console.log("*******************************");
+console.log("Game Over");
+console.log("you got " + scores + " out of  " + quizQuestion.length);
+console.log("********************************");
+
+console.log("");
+console.log("Thanks for playing ");
+
+function displayQuestion(item, index) {
+  let optionsItemNumber = 0;
+
   console.log(" ");
 
-  let optionsItemNumber = 0;
+  console.log(index + 1 + ". " + item.question);
+  console.log("");
+
   for (let charCode = 65; charCode <= 68; charCode++) {
     let char = String.fromCharCode(charCode);
     console.log(char + "." + item.options[optionsItemNumber]);
     optionsItemNumber++;
   }
   console.log(" ");
+}
 
-  const userInput = prompt();
-  let userAnswer;
-  switch (userInput) {
+function readAnswer(input, item) {
+  switch (input.toUpperCase()) {
     case "A":
-      userAnswer = item.options[0];
+      return item.options[0];
       break;
     case "B":
-      userAnswer = item.options[1];
+      return item.options[1];
       break;
     case "C":
-      userAnswer = item.options[2];
+      return item.options[2];
       break;
     case "D":
-      userAnswer = item.options[3];
+      return item.options[3];
       break;
     default:
       console.log("Incorrect input ");
+      return null;
       break;
   }
+}
 
-  if (userAnswer == item.correctAnswer) scores++;
-
-  console.log(userAnswer);
-});
-
-console.log("Game Over");
-console.log("you got " + scores + " out of 5");
-
-console.log("Thanks for playing ");
+function checkInputValidate(input) {
+  const vaildInputs = ["A", "B", "C", "D", "Q"];
+  return vaildInputs.includes(input.toUpperCase());
+}
---
+++ data.js
@@ -0,0 +1,43 @@
+const quizQuestion = [
+  {
+    question:
+      "Which programming language is known for its use in web development and is commonly paired with Node.js?",
+    options: ["Python", "Java", "JavaScript", "C++"],
+    correctAnswer: "JavaScript",
+  },
+  {
+    question:
+      "In 'The Walking Dead,' who is the leader of the group at the beginning of the series?",
+    options: ["Rick Grimes", "Daryl Dixon", "Michonne", "Glenn Rhee"],
+    correctAnswer: "Rick Grimes",
+  },
+  {
+    question: "Who was the first president of the United States?",
+    options: [
+      "Thomas Jefferson",
+      "John Adams",
+      "George Washington",
+      "Abraham Lincoln",
+    ],
+    correctAnswer: "George Washington",
+  },
+  {
+    question:
+      "Which book of the Bible tells the story of the creation of the world?",
+    options: ["Exodus", "Genesis", "Leviticus", "Numbers"],
+    correctAnswer: "Genesis",
+  },
+
+  {
+    question: "During which war was the Battle of Gettysburg fought?",
+    options: [
+      "World War I",
+      "World War II",
+      "American Civil War",
+      "Vietnam War",
+    ],
+    correctAnswer: "American Civil War",
+  },
+];
+
+module.exports = quizQuestion;
